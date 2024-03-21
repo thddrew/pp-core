31c04454fca6bfc8fda456e9e6e60ab47d312a83
thddrew <thddrew@gmail.com>
2024-03-20 22:23:55 -0700

    fix: remove dead code

--- app/(root)/playground/page.tsx
+++ app/(root)/playground/page.tsx
@@ -1,20 +1,6 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { getUsers } from "@/lib/prisma/queries/users";
-import { getCore } from "@/sanity/queries/core";
+import { Table, TableBody, TableCaption, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default async function Playground() {
-  const coreItems = await getCore();
-  const users = await getUsers();
-
   return (
     <section className="w-full p-4">
       <Table>
@@ -25,26 +11,6 @@ export default async function Playground() {
             <TableHead>Description</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
-          {coreItems.map((item) => (
-            <TableRow key={item._id}>
-              <TableCell className="font-medium">{item.title}</TableCell>
-              <TableCell>{item.description}</TableCell>
-            </TableRow>
-          ))}
-          {users.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell className="font-medium">{user.clerkId}</TableCell>
-              <TableCell>{user.plaidAccountId}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell>Total</TableCell>
-            <TableCell className="text-right">{coreItems.length}</TableCell>
-          </TableRow>
-        </TableFooter>
       </Table>
     </section>
   );
