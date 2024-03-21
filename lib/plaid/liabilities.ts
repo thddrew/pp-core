233c814fd467f80e25e90c8c1bd176bc53789c61
thddrew <thddrew@gmail.com>
2024-03-20 23:08:44 -0700

    fix: comment out old code

--- lib/plaid/liabilities.ts
+++ lib/plaid/liabilities.ts
@@ -8,16 +8,16 @@ export const getLiabilities = async (plaidAccountId: number) => {
   const plaidAccount = await getAccountById(plaidAccountId);
 
   if (!plaidAccount) throw new Error("Plaid account not found");
-  if (!plaidAccount.access_token) throw new Error("Plaid account access token not found");
+  // if (!plaidAccount.access_token) throw new Error("Plaid account access token not found");
 
-  try {
-    const response = await PlaidClient.liabilitiesGet({
-      access_token: plaidAccount.access_token,
-    });
+  // try {
+  //   const response = await PlaidClient.liabilitiesGet({
+  //     access_token: plaidAccount.access_token,
+  //   });
 
-    return response.data;
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+  //   return response.data;
+  // } catch (err) {
+  //   console.log(err);
+  //   throw err;
+  // }
 };
